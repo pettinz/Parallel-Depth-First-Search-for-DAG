c0661b571a3ad3db0313ed334d93ae306e20b935
Angelo Pettinelli <angelo.pettinelli@icloud.com>
2020-08-31 16:04:34 +0200

    main

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#define N_ARGS 2
+
+#include <iostream>
+
+#include "spdlog/spdlog.h"
+#include "dag.hpp"
+
+using namespace std;
+
+void usage(const char *);
+
+int main(int argc, const char *argv[])
+{
+    if (argc < N_ARGS)
+    {
+        usage(argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    spdlog::set_level(spdlog::level::info);
+
+    DAG dag;
+    dag.readFromFile(argv[1]);
+
+    return 0;
+}
+
+void usage(const char *progName)
+{
+    cout << "usage: ";
+    cout << progName << " file1 file2" << endl << endl;
+}
