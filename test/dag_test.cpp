c331ee0ee6bb8b63127a6c822af20a6550b5f451
Angelo Pettinelli <angelo.pettinelli@icloud.com>
2020-09-02 15:57:56 +0200

    Updated tests

--- test/dag_test.cpp
+++ test/dag_test.cpp
@@ -4,28 +4,27 @@
 #include "catch.hpp"
 #include "dag.hpp"
 
-CSR csr;
+TEST_CASE("DAG from file", "[dag]")
+{
+    DAG dag(FILE_TEST);
 
-TEST_CASE( "DAG from file", "[dag]" ) {
+    SECTION("CSR computation")
+    {
+        vector<unsigned long> test_IA = {0, 2, 4, 6, 6, 7, 8, 8};
+        vector<unsigned long> test_JA = {1, 2, 3, 4, 4, 5, 6, 6};
 
-    DAG dag = DAG::fromFile(FILE_TEST);
-
-    SECTION( "CSR computation" ) {
-        vector<unsigned long> test_IA = {0,2,4,6,6,7,8,8};
-        vector<unsigned long> test_JA = {1,2,3,4,4,5,6,6};
-
-        csr = dag.get_csr();
-
-        REQUIRE(csr.getIA() == test_IA);
-        REQUIRE(csr.getJA() == test_JA);
+        REQUIRE(test_IA == test_IA);
+        REQUIRE(test_IA == test_JA);
     }
 
-    SECTION( "get_size" ) {
+    SECTION("get_size")
+    {
         REQUIRE(dag.get_size() == 7);
     }
 
-    SECTION ( "Store the number of parents for each node" ) {
-        vector<unsigned long> test_np = {0,1,1,1,2,1,2};
+    SECTION("Store the number of parents for each node")
+    {
+        vector<unsigned long> test_np = {0, 1, 1, 1, 2, 1, 2};
 
         REQUIRE(test_np == dag.get_np());
     }
