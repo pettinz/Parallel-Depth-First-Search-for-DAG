08bec4bc0936098e499c8af4a0f7cdee9228d5c9
Lorenzo Pintore s276315 <s276315@studenti.polito.it>
2020-09-09 12:26:54 +0200

    merge local

--- test/test.cpp
+++ test/test.cpp
@@ -1,103 +1,72 @@
 #define CATCH_CONFIG_MAIN
-
-#ifndef GRA_DIR
-#define GRA_DIR ""
-#endif
-#define TEST_PARALLEL_DFS
-#define TEST_LABELING
-
-#include <string>
-#include <queue>
-#include <filesystem>
-namespace fs = std::filesystem;
+#define private public
 
 #include "catch.hpp"
 #include "spdlog/spdlog.h"
 #include "dag.hpp"
 
+#include <filesystem>
+#include "amaze.gra.hpp"
+
 using namespace std;
 
-void getTestFiles(const string &dir, queue<string> &files)
+DAG dag(TEST_FILE);
+
+#ifdef TEST_READ
+TEST_CASE("Read", "[dag]")
 {
-    queue<string> files_tmp;
-
-    for (auto &p : fs::directory_iterator(dir))
-        if (fs::is_regular_file(p) && p.path().extension() == ".gra")
-            files_tmp.push(p.path().string());
-
-    files = move(files_tmp);
+    REQUIRE(dag.getV() == test_V);
+    REQUIRE(dag.getIA() == test_IA);
+    REQUIRE(dag.getJA() == test_JA);
+    REQUIRE(dag.get_np() == test_np);
 }
+#endif // TEST_READ
 
-void getTestFilesRecursive(const string &dir, queue<fs::path> &files)
+TEST_CASE("DFS", "[dag]")
 {
-    queue<fs::path> files_tmp;
+    DAG::DT dt = dag.toDT();
 
-    for (auto &p : fs::recursive_directory_iterator(dir))
-        if (fs::is_regular_file(p) && p.path().extension() == ".gra")
-            files_tmp.push(p.path().string());
+    const vector<unsigned long> &IA = dt.getIA(), &JA = dt.getJA(), &parents = dt.getParents();
 
-    files = move(files_tmp);
-}
-
-#ifdef TEST_PARALLEL_DFS
-TEST_CASE("parallelDFS", "f")
-{
-    queue<fs::path> files;
-    getTestFilesRecursive(GRA_DIR, files);
-
-    spdlog::info("Testing parallel DFS");
-    while (!files.empty())
+#ifdef SECTION_1
+    SECTION("Directed tree generation")
     {
-        fs::path file = files.front();
-        files.pop();
+        REQUIRE(parents == test_dtParents);
+        REQUIRE(IA == test_dtIA);
+        REQUIRE(JA == test_dtJA);
+    }
+#endif // SECTION_1
 
-        spdlog::info("...on file {}...", file.filename().string());
+    vector<unsigned long> subgraphSize, presum;
+    dt.computeNodeSizeAndPresum(subgraphSize, presum);
 
-        DAG dag(file.string());
+#ifdef SECTION_2
+    SECTION("Subgraph size computation")
+    {
+        REQUIRE(subgraphSize == test_subgraph);
+        REQUIRE(presum == test_presum);
+    }
+#endif // SECTION_2
 
-        vector<unsigned long> preorder[2], postorder[2], innerRank, outerRank;
+    vector<unsigned long> preorder, postorder, inner;
+    dt.parallelDFS(preorder, postorder);
 
-        dag.DFS(preorder[0], postorder[0], innerRank, outerRank);
+#ifdef SECTION_3
+    SECTION("Pre- and post-order")
+    {
+        REQUIRE(preorder == test_preorder);
+        REQUIRE(postorder == test_postorder);
+    }
+#endif // SECTION_3
 
-        dag.parallelDFS(preorder[1], postorder[1]);
+    dag.labeling(postorder, inner);
+    SECTION("Parallel vs Recursive")
+    {
+        vector<unsigned long> preorder_r, postorder_r, inner_r, outer_r;
+        dag.DFS(preorder_r, postorder_r, inner_r, outer_r);
 
-        REQUIRE(preorder[0] == preorder[1]);
-        REQUIRE(postorder[0] == postorder[1]);
-
-        spdlog::info("\tcompleted.");
+        REQUIRE(preorder == preorder_r);
+        REQUIRE(postorder == outer_r);
+        REQUIRE(inner == inner_r);
     }
 }
-#endif
-
-#ifdef TEST_LABELING
-TEST_CASE("labeling", "dag")
-{
-    queue<fs::path> files;
-    getTestFilesRecursive(GRA_DIR, files);
-
-    spdlog::info("Testing labeling");
-    while (!files.empty())
-    {
-        fs::path file = files.front();
-        files.pop();
-
-        spdlog::info("...on file {}...", file.filename().string());
-
-        DAG dag(file);
-
-        vector<unsigned long> preorder, postorder, outerRank[2], innerRank[2];
-
-        dag.DFS(preorder, postorder, innerRank[1], outerRank[1]);
-
-        auto start = chrono::high_resolution_clock::now();
-        ios_base::sync_with_stdio(false);
-        dag.labeling(outerRank[0], innerRank[0]);
-        auto end = chrono::high_resolution_clock::now();
-
-        REQUIRE(outerRank[0] == outerRank[1]);
-        REQUIRE(innerRank[0] == innerRank[1]);
-
-        spdlog::info("\tcompleted.");
-    }
-}
-#endif
